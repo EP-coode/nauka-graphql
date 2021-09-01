858ec61320e55f04624585a255638ec2c233fb97
EP-coode <68182419+EP-coode@users.noreply.github.com>
2021-09-01 12:22:34 +0200

    data and types

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 const express = require('express')
-const { graphqlHTTP  } = require('express-graphql')
+const { graphqlHTTP } = require('express-graphql')
 const { buildSchema } = require('graphql')
 
 const app = express()
 
 app.use(express.json())
 
-const events = ["bob's chalange", "yee", "test", "bannannana"]
+const events = []
 
 app.use('/graphql', graphqlHTTP({
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput{
+            title: String!
+            description: String!
+            price: Float!
+        }
+
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
 
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -28,13 +42,17 @@ app.use('/graphql', graphqlHTTP({
             return events
         },
         createEvent: (args) => {
-            const eventName = args.name
-            events.push(eventName)
-            return eventName
+            const event = {
+                _id: Math.random().toString(),
+                ...args.eventInput,
+                date: new Date().toISOString(),
+            }
+            events.push(event)
+            return event
         }
     },
     graphiql: true
-})) 
+}))
 
 app.listen(3000, () => {
     console.log(`Listening on http://localhost:3000`);
