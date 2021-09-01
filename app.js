8ad7bfe19341297e6b718616242f8b87bf660292
EP-coode <68182419+EP-coode@users.noreply.github.com>
2021-09-01 11:53:35 +0200

    basic graphql setup

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 const express = require('express')
+const { graphqlHTTP  } = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res, next) => {
-    
-    res.send('Hello there yee')
-})
+const events = ["bob's chalange", "yee", "test", "bannannana"]
+
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return events
+        },
+        createEvent: (args) => {
+            const eventName = args.name
+            events.push(eventName)
+            return eventName
+        }
+    },
+    graphiql: true
+})) 
 
 app.listen(3000, () => {
     console.log(`Listening on http://localhost:3000`);
