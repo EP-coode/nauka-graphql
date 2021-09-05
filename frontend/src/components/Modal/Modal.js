f307703068f104239ceca4a440f26aef2beb9474
EP-coode <68182419+EP-coode@users.noreply.github.com>
2021-09-05 18:29:14 +0200

    modal done

--- frontend/src/components/Modal/Modal.js
+++ frontend/src/components/Modal/Modal.js
@@ -3,12 +3,12 @@ import React from 'react';
 import './Modal.css'
 
 const Modal = ({ title, children, onCancel, onConfirm }) => (
-    <div className="modaol">
-        <header>{title}</header>
+    <div className="modal">
+        <header className="modal__header"r>{title}</header>
         <section className="modal__content">
             {children}
         </section>
-        <section className="modal__content">
+        <section className="modal__actions">
             {onCancel && <button className="modal__btn btn" onClick={onCancel}>Cancel</button>}
             {onConfirm && <button className="modal__btn btn" onClick={onConfirm}>Confirm</button>}
         </section>
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,43 +1,63 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+
+import Modal from '../components/Modal/Modal';
+import Backdrop from '../components/Backdrop/Backdrop';
+import './Events.css'
 
 function EventsPage() {
     const inputTitle = useRef(null)
     const inputDesc = useRef(null)
     const inputPrice = useRef(null)
     const inputDate = useRef(null)
+    const [creatingEvent, setCreatingEvent] = useState(false)
 
-    const handleFormSubit = e => {
+    const createEventClickHandler = e => {
         e.preventDefault()
+        setCreatingEvent(true)
+    }
+
+    const modalConfirmHandler = e => {
+        setCreatingEvent(false)
+    }
+
+    const modalCancelHandler = e => {
+        setCreatingEvent(false)
     }
 
     return (
         <div className="events">
-            <h1>
-                Events page
-            </h1>
-            <form className="form">
-                <div className="form__control">
-                    <label htmlFor="title">E-mail</label>
-                    <input type="text" id="title" ref={inputTitle}></input>
-                </div>
-                <div className="form__control">
-                    <label htmlFor="description">password</label>
-                    <input type="text" id="description" ref={inputDesc}></input>
-                </div>
-                <div className="form__control">
-                    <label htmlFor="price">password</label>
-                    <input type="number" id="price" ref={inputPrice}></input>
-                </div>
-                <div className="form__control">
-                    <label htmlFor="date">password</label>
-                    <input type="date" id="date" ref={inputDate}></input>
-                </div>
-                <div className="form__control">
-                    <button className="btn" type="button" onClick={handleFormSubit}>
-                        Create Event
-                    </button>
-                </div>
-            </form>
+            {creatingEvent && <Backdrop />}
+            {
+                creatingEvent &&
+                <Modal
+                    title="Title"
+                    onCancel={modalCancelHandler}
+                    onConfirm={modalConfirmHandler}
+                >
+                    {/* <form className="form">
+                    <div className="form__control">
+                        <label htmlFor="title">E-mail</label>
+                        <input type="text" id="title" ref={inputTitle}></input>
+                    </div>
+                    <div className="form__control">
+                        <label htmlFor="description">password</label>
+                        <input type="text" id="description" ref={inputDesc}></input>
+                    </div>
+                    <div className="form__control">
+                        <label htmlFor="price">password</label>
+                        <input type="number" id="price" ref={inputPrice}></input>
+                    </div>
+                    <div className="form__control">
+                        <label htmlFor="date">password</label>
+                        <input type="date" id="date" ref={inputDate}></input>
+                    </div>
+                </form> */}
+                    modal content
+                </Modal>
+            }
+            <button className="btn" type="button" onClick={createEventClickHandler}>
+                Create Event
+            </button>
 
         </div>
     )
